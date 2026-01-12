a92663474ba49dd0892373ac9355a7fb61d4efec
Denzel Anoliefo <167828830+DenzelAnoliefo@users.noreply.github.com>
2026-01-12 10:27:12 -0500

    Updated Covert Logic
    changed followers/following extraction from JSON to python list.

--- src/convert.py
+++ src/convert.py
@@ -1,1 +1,30 @@
+import json
+
+with open('followers_1.json', 'r') as f:
+    data_followers = json.load(f)
+
+with open('following.json', 'r') as f:
+    data_following = json.load(f)
+
+
+followers = []
+following = [] 
+
+
+# followers
+for acct in range(len(data_followers)):
+    followers.append(data_followers[acct]['string_list_data'][0]['value'])
+
+# following
+for acct in data_following['relationships_following']:
+    usr = acct['title']
+    following.append(usr)
+
+
+unfollow = list(set(following) - set(followers))
+
+print("Followers count:", len(followers))
+print("Following count:", len(following)) 
+
+print(unfollow)
 
